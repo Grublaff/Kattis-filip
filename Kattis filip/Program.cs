fe6ab13fb7d458f868e10538dc81c14487877e79
Grubla <filip.aburg@gmail.com>
2021-06-08 23:25:10 +0200

    Add project files.

---
+++ Kattis filip/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kattis_filip
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string temp = input[i];
+                char[] charArray = temp.ToCharArray();
+                Array.Reverse(charArray);
+                temp = new string(charArray);
+                list.Add(Int32.Parse(temp));
+            }
+            list.Sort();
+            Console.WriteLine(list[1]);
+        }
+    }
+}
